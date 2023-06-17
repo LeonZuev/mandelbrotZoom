83252a6e2464137753145b01993d851ed102dba2
leonzuev <loonatune@gmail.com>
2023-06-17 23:40:12 +0300

    Initial commit

---
+++ scetch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(640, 360);
+
+    noStroke();
+    colorMode(HSB);
+}
+
+let cenX = 0;
+let cenY = 0;
+let scale = 1;
+
+function draw () {
+
+}
+
+function drawBrot() {
+    
+}
